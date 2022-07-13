57c97ea9e964d4f53f019bac0250097ac3127112
bhushan-patil-12 <bhushanpatil833@gmail.com>
2022-07-13 13:31:21 +0530

    Added Account model

---
+++ StudentInformationSystem/src/model/Account.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Account {
+	int accountNo;
+	double balance;
+
+	public int getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+}
